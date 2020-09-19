be85154a4924fc51a7b8fdd85a08a415ac629f76
Linh Pham <linhphaaam@gmail.com>
2020-09-19 12:42:56 -0400

    add and style tooltip to show tree names on hover

--- js/script.js
+++ js/script.js
@@ -2,63 +2,11 @@
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
 
-// d3.csv("../Trees_Lat_Long.csv").then(function(data) {
-//     console.log(data[0]);
-// });
-
 mapboxgl.accessToken = 'pk.eyJ1IjoibGluaHBoYWFhbSIsImEiOiJja2Y4c2Z5N3kwOThuMnRtaHczZ3dnbWFmIn0.8ABZB_jFyTWSj7hd_6Gj5w';
 
 var geojson = {
     'type': 'FeatureCollection',
-    'features': [
-        // {
-        //     'type': 'Feature',
-        //     'properties': {
-        //         'message': 'Tree 1',
-        //         'iconSize': [40, 40]
-        //     },
-        //     'geometry': {
-        //         'type': 'Point',
-        //         'coordinates': [-122.3837697, 37.7441324]
-        //     }
-        // },
-
-        // {
-        //     'type': 'Feature',
-        //     'properties': {
-        //         'message': 'Tree 2',
-        //         'iconSize': [40, 40]
-        //     },
-        //     'geometry': {
-        //         'type': 'Point',
-        //         'coordinates': [-122.3239636, 37.5535689]
-        //     }
-        // },
-
-        // {
-        //     'type': 'Feature',
-        //     'properties': {
-        //         'message': 'Tree 3',
-        //         'iconSize': [40, 40]
-        //     },
-        //     'geometry': {
-        //         'type': 'Point',
-        //         'coordinates': [-122.4342911, 37.7371298]
-        //     }
-        // },
-
-        // {
-        //     'type': 'Feature',
-        //     'properties': {
-        //         'message': 'Tree 4',
-        //         'iconSize': [40, 40]
-        //     },
-        //     'geometry': {
-        //         'type': 'Point',
-        //         'coordinates': [-122.4342911, 37.7371298]
-        //     }
-        // }
-    ]
+    'features': []
 };
 
 d3.csv("../Trees_Lat_Long.csv").then(function(data) {
@@ -69,7 +17,7 @@ d3.csv("../Trees_Lat_Long.csv").then(function(data) {
         feature = {};
         feature['type'] = 'Feature'
         feature['properties'] = {
-            'message': 'Tree type: ' + d.Tree,
+            'name': d.Tree,
             'iconSize': [30, 30]
         },
         feature['geometry'] = {
@@ -85,22 +33,30 @@ d3.csv("../Trees_Lat_Long.csv").then(function(data) {
         // create a DOM element for the marker
         var el = document.createElement('div');
         el.className = 'marker';
+
+        var tooltip = document.createElement('div');
+        tooltip.className = 'tooltip';
+        el.appendChild(tooltip);
+
+        var tooltiptext = document.createElement('span');
+        tooltiptext.className = 'tooltiptext';
+        tooltip.appendChild(tooltiptext);
+
+        tooltiptext.innerHTML = marker.properties.name;
+        
         el.style.width = marker.properties.iconSize[0] + 'px';
         el.style.height = marker.properties.iconSize[1] + 'px';
     
-        el.addEventListener('click', function () {
-            window.alert(marker.properties.message);
-            });
+        // el.addEventListener('click', function () {
+        //     window.alert(marker.properties.message);
+        //     });
     
         new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
-        // new mapboxgl.Marker(el).setLngLat([-122.3239636, 37.5535689]).addTo(map);
-        // new mapboxgl.Marker(el).setLngLat([-122.4407008, 37.7621085]).addTo(map);
-        // new mapboxgl.Marker(el).setLngLat([-122.4342911, 37.7371298]).addTo(map);
     
     });
 });
 
-console.log(geojson);
+// console.log(geojson);
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -115,25 +71,6 @@ accessToken: mapboxgl.accessToken
 }),
 'top-left'
 );
-
-
-// geojson.features.forEach(function (marker) {
-//     // create a DOM element for the marker
-//     var el = document.createElement('div');
-//     el.className = 'marker';
-//     el.style.width = marker.properties.iconSize[0] + 'px';
-//     el.style.height = marker.properties.iconSize[1] + 'px';
-
-//     el.addEventListener('click', function () {
-//         window.alert(marker.properties.message);
-//         });
-
-//     new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
-//     // new mapboxgl.Marker(el).setLngLat([-122.3239636, 37.5535689]).addTo(map);
-//     // new mapboxgl.Marker(el).setLngLat([-122.4407008, 37.7621085]).addTo(map);
-//     // new mapboxgl.Marker(el).setLngLat([-122.4342911, 37.7371298]).addTo(map);
-
-// });
 
 map.addControl(
 new mapboxgl.GeolocateControl({
